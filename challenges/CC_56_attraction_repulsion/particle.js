3a722429f942b09b7ba1a92fa825821d16c0031e
Aaron <a.meredith@we-are-mea.com>
2017-02-10 18:12:13 +1300

    a

--- challenges/CC_56_attraction_repulsion/particle.js
+++ challenges/CC_56_attraction_repulsion/particle.js
@@ -7,21 +7,41 @@ function Particle(x, y) {
   this.pos = createVector(x, y);
   this.prev = createVector(x, y);
   this.vel = createVector(); //p5.Vector.random2D();
+  this.trail = [];
   //this.vel = p5.Vector.random2D();
   //this.vel.setMag(random(2, 5));
   this.acc = createVector();
 
+  var setTrail = 0;
   this.update = function() {
     this.vel.add(this.acc);
     this.vel.limit(5);
     this.pos.add(this.vel);
+    
+    //if(!(++setTrail%2)){
+      this.trail.push(createVector(this.pos.x, this.pos.y));
+    //}
+    
+    if(this.trail.length > 20){
+      this.trail.splice(0, 1);
+    }
+
     this.acc.mult(0);
   }
 
   this.show = function() {
-    stroke(255, 255);
-    strokeWeight(4);
-    line(this.pos.x, this.pos.y, this.prev.x, this.prev.y);
+    noFill();
+    stroke(255, 5);    
+    strokeWeight(1);
+
+    beginShape();
+      for (var k = 0; k < this.trail.length; k++) {
+        var trailPoint = this.trail[k];
+        curveVertex(trailPoint.x,  trailPoint.y);
+      }
+    endShape();
+
+   // line(this.pos.x, this.pos.y, this.prev.x, this.prev.y);
 
     this.prev.x = this.pos.x;
     this.prev.y = this.pos.y;
@@ -33,11 +53,11 @@ function Particle(x, y) {
     var force = p5.Vector.sub(target, this.pos);
     var d = force.mag();
     d = constrain(d, 1, 25);
-    var G = 50;
+    var G = 5;
     var strength = G / (d * d);
     force.setMag(strength);
-    if (d < 20) {
-      force.mult(-10);
+    if (d < 5) {
+      force.mult(-5);
     }
     this.acc.add(force);
   }
--- challenges/CC_56_attraction_repulsion/sketch.js
+++ challenges/CC_56_attraction_repulsion/sketch.js
@@ -7,8 +7,16 @@ var attractors = [];
 var particles = [];
 
 function setup() {
-  createCanvas(400, 400);
 
+  var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+  createCanvas(x, y);
+  background(51);
   // for (var i = 0; i < 10; i++) {
   //   attractors.push(createVector(random(width), random(height)));
   // }
@@ -18,25 +26,45 @@ function mousePressed() {
   attractors.push(createVector(mouseX, mouseY));
 }
 
+var drawBG = 0;
+
 function draw() {
-  background(51);
+  
+  if(!(++drawBG%600)){
+    background(51);
+  }
+
+  //background(51);
+
   stroke(255);
   strokeWeight(4);
-  particles.push(new Particle(random(width), random(height)));
+  if (particles.length < 200) {
+    if(attractors.length){
+      var spawn = attractors[ Math.floor(Math.random()*attractors.length) ]; 
+      
+      particles.push(new Particle(spawn.x + random(-20,20), spawn.y + random(-20,20)));
+    }
 
-  if (particles.length > 100) {
-    particles.splice(0, 1);
+  
+    //particles.splice(0, 1);
   }
 
   for (var i = 0; i < attractors.length; i++) {
     stroke(0, 255, 0);
     point(attractors[i].x, attractors[i].y);
   }
+  
   for (var i = 0; i < particles.length; i++) {
     var particle = particles[i];
     for (var j = 0; j < attractors.length; j++) {
       particle.attracted(attractors[j]);
     }
+
+    for (var m = 0; m < particles.length; m++) {
+      particle.attracted(particles[m].pos);
+    }
+
+
     particle.update();
     particle.show();
   }
